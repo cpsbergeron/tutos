9a4e4f60d78e08235b48f092ecd4f604464fc383
cpsbergeron <161048820+cpsbergeron@users.noreply.github.com>
2025-02-07 10:07:15 -0500

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(Math.round(0 * 0))
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 2; index++) {
+    	
+    }
+})
+basic.showNumber(input.lightLevel())
+basic.showNumber(input.temperature())
+basic.pause(100)
